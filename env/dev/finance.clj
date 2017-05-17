76c49f4ee58cca8c35ac02bccccfb78c20fce757
Matthew Marks <matthewfmarks@gmail.com>
2017-05-17 15:15:45 -0400

    wip can get sequence of assets and liabilities
    try it yourself:
    (take 120 (advance-a-month good bad) after evaling things.
    you'll see 10 years of payments.
    TODO: remove loans once they're paid

--- env/dev/finance.clj
+++ env/dev/finance.clj
@@ -1,52 +1,41 @@
 (ns flierplath.finance)
 
-;; can have either amount and interest XOR income
-(def assets [{:name "cash" :amount 5000 :interest 0.07}
-             {:name "house" :amount 100000 :interest 0.03}
-             {:name "salary" :income 60000}
-             ])
+(def good {:incomes [{:name "rental-income" :income 6000}
+                     {:name "salary" :income 50000}]
+           :cash {:name "vanguard" :amount 5000 :interest 0.07}
+           :assets [{:name "house" :amount 100000 :interest 0.07}
+                    {:name "rental-house" :amount 50000 :interest 0.07}]})
 
-;; if we want "undo" and historical data, here's an obvious case:
-(def liabs [{:name "car-loan" :amount 20000 :interest 0.07 :payment 500}
-            {:name "school-loan" :amount 50000 :interest 0.04 :payment 1000}
-            {:name "groceries" :payment 600}
-            {:name "clothing" :payment 200}
-            {:name "electric bill" :payment 100}
-            ])
+(def bad {:loans [{:name "car-loan" :amount 20000 :interest 0.07 :payment 6000}
+                  {:name "school-loan" :amount 50000 :interest 0.04 :payment 12000}]
+          :consumables [{:name "groceries" :payment 7200}
+                        {:name "clothing" :payment 2400}
+                        {:name "electric bill" :payment 1200}]})
 
-(defn calc-an-assets-income [{:keys [:name :amount :interest :income]}]
-  (if (nil? income)
-    (- (* (+ 1 (/ interest 12)) amount) amount)
-    (/ income 12)))
+(defn calculate-surplus [{:keys [incomes cash] :as g} {:keys [loans consumables] :as b}]
+  (let [monthly-income (/ (reduce + (map :income incomes)) 12)
+        monthly-expenses (/ (reduce + (concat (map :payment loans) (map :payment consumables))) 12)] ;; TODO calculate cash income
+    (- monthly-income monthly-expenses)))
 
-(defn calc-months-income [a]
-  (reduce + (map calc-an-assets-income a)))
+(defn adv [{:keys [amount interest] :as m}]
+  (assoc m :amount (* amount (+ 1 (/ interest 12)))))
 
-(calc-months-income assets)
+(defn advance-good [{:keys [cash assets] :as good} surplus]
+  (let [n-assets (map adv assets)
+        n-cash (adv (assoc cash :amount (+ surplus (:amount cash))))]
+    (assoc good :assets n-assets :cash n-cash)))
 
-                                        ; when big is bigger than small
-(take-while #(< 0 (:vla %)) [{:vla 1} {:vla 2} {:vla -1}])
+(defn apply-payments [{:keys [amount payment] :as loan}]
+  (let [monthly-payment (/ payment 12)]
+    (assoc loan :amount (- amount monthly-payment))))
 
-(defn lazy-ass [{:keys [:asset :debt]}]
-  (cons {:asset asset :debt debt} (lazy-seq (lazy-ass {:asset (+ asset asset) :debt debt}))))
-(take-while #(> 500 (:asset %)) (lazy-ass {:asset 100 :debt 100}))
-(lazy-ass {:asset 2 :debt 0})
+(defn advance-bad [{:keys [loans] :as bad}]
+  (let [with-payments (map apply-payments loans)
+        n-loans (map adv with-payments)]
+    (assoc bad :loans n-loans)))
 
-(defn decrement-liabs [liabs]
-  (let [ (map decrement-a-liab (filter (nil? ) liabs))])
-  (keep-if-not-nil liabs-without-zeros))
-
-;; don't *do* anything with consumable expenses. just add them up later.
-(filter #(nil? (:amount %) ) liabs)
-
-(defn decrement-a-liab [{:keys [:amount :payment] :as liab}]
-  (assoc liab :amount (- amount payment)))
-
-(decrement-a-liab (first liabs))
-
-(defn advance-a-month [assets liabs]
-  (let [recalculated-assets (compute assets)
-        decremented-liabs (decrement-liabs liabs)
-        ]
-    (cons [assets liabs] (lazy-seq (advance-a-month recalculated-assets decremented-liabs)))
-    ))
+(defn advance-a-month [g b]
+  (let [surplus (calculate-surplus g b)
+        n-good (advance-good g surplus)
+        n-bad (advance-bad b)]
+    (cons [g b] (lazy-seq (advance-a-month n-good n-bad)))))
