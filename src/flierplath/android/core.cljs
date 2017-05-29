d1577f56f898a8e80c8a1a51466ef116e3190cb1
Matthew Marks <matthewfmarks@gmail.com>
2017-05-29 19:45:26 -0400

    android compilation errors https://stackoverflow.com/a/44220973 https://github.com/airbnb/lottie-react-native/issues/38

--- src/flierplath/android/core.cljs
+++ src/flierplath/android/core.cljs
@@ -12,7 +12,7 @@
 (def image (r/adapt-react-class (.-Image ReactNative)))
 (def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
 
-(def logo-img (js/require "./images/cljs.png"))
+#_(def logo-img (js/require "./images/cljs.png"))
 
 (defn alert [title]
       (.alert (.-Alert ReactNative) title))
@@ -22,7 +22,7 @@
     (fn []
       [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
        [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
-       [image {:source logo-img
+       #_[image {:source logo-img
                :style  {:width 80 :height 80 :margin-bottom 30}}]
        [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
                              :on-press #(alert "HELLO!")}
